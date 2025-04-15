2d712fe564e84483a186d6d20f056078eb6189bd
mummumm <01088287493@daum.net>
2025-04-15 15:46:07 +0900

    ♻️ refactor [#1] : UserReadResponse에서 userId 제거

--- src/main/java/com/sparta/limited/order_service/infrastructure/client/dto/UserReadResponse.java
+++ src/main/java/com/sparta/limited/order_service/infrastructure/client/dto/UserReadResponse.java
@@ -1,18 +1,14 @@
 package com.sparta.limited.order_service.infrastructure.client.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class UserReadResponse {
 
-    @JsonProperty("id")
-    private final Long userId;
     private final String username;
     private final String address;
 
-    public UserReadResponse(Long userId, String username, String address) {
-        this.userId = userId;
+    public UserReadResponse(String username, String address) {
         this.username = username;
         this.address = address;
     }
